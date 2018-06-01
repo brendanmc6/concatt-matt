33a505fca01a5de55c5439bd9d353f3d49c50d2b
Brendanmc6 <brendanmc6@gmail.com>
2018-06-01 02:09:33 +0200

    init-commit 4 concatt-matt

---
+++ index.js
@@ -0,0 +1,67 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { secret_code } = require("./secret");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let matt = {
+  justPosted: false,
+  timer: null,
+  recentMessages: []
+};
+
+const concatAndSend = lastMsg => {
+  let content = [];
+  matt.recentMessages.forEach((message, index) => {
+    if (
+      //already punctuated, do nothing
+      /[^\w\s]/.test(message.content.slice(-1))
+    ) {
+      content.push(message.content);
+    } else if (
+      //last message, or long message, add period.
+      index === matt.recentMessages.length - 1 ||
+      message.content.length >= 50
+    ) {
+      content.push(message.content + ".");
+    } else if (message.content.length >= 10) {
+      content.push(message.content + ",");
+    } else {
+      content.push(message.content);
+    }
+    message.delete();
+  });
+  lastMsg.channel.send(content.join(" "));
+};
+
+const timerEnd = lastMsg => {
+  if (matt.recentMessages.length >= 2) {
+    concatAndSend(lastMsg);
+  }
+  matt.justPosted = false;
+  matt.recentMessages = [];
+};
+
+const startTimer = msg => {
+  matt.justPosted = true;
+  matt.recentMessages.push(msg);
+  matt.timer = setTimeout(timerEnd, 10000, msg);
+};
+
+//every time somebody posts a message
+client.on("message", msg => {
+  //and that somebody was matt
+  if (msg.author.discriminator === "0535") {
+    //and if matt had just posted, reset the timer and save the message
+    if (matt.justPosted) {
+      clearTimeout(matt.timer);
+    }
+    startTimer(msg);
+  } else if (!msg.author.bot) {
+    timerEnd(msg);
+  }
+});
+
+client.login("NDUxODE2OTIyMDY2MTkwMzUx.DfHikQ.24br2H6B_dAP1U-kjGjeoI4KjTU");
